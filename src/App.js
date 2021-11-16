e39a7d728d5e8e732624feea5fb8730873f4330a
shreya1010svg <shrey1010.svg@gmail.com>
2021-11-16 16:48:54 +0530

    Search for title prototype

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import JSONDATA from './MOCK_DATA.json'
+import JSONDATA from './MOCK_DATA.json';
+import {useState} from 'react';
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('')
   return (
 	<div className="App">
-		<input type="text" placeholder="Search..."/>
-		{JSONDATA.map((val,key)=>{
+		<input type="text" placeholder="Search..." onChange={event => {setSearchTerm(event.target.value)}}/>
+		{JSONDATA.filter((val) =>
+				{
+					if(searchTerm == "")
+					{
+						return val
+					}
+					else if(val.title.toLowerCase().includes(searchTerm.toLowerCase()))
+					{
+						return val
+					}
+				}).map((val,key)=>{
 			return (
 				<div className="file" key={key}>
 					<p>{val.title}</p>
